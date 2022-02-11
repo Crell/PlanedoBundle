fd6474306f69ee3f999f2985b6036baeb9ad4a55
Larry Garfield <larry@garfieldtech.com>
2022-02-11 15:09:21 -0600

    Return the mocked clock so it can be reset as needed in a test.

--- tests/Functional/MockClockTrait.php
+++ tests/Functional/MockClockTrait.php
@@ -12,16 +12,19 @@ namespace Crell\Bundle\Planedo\Tests\Functional;
 
 use Crell\Bundle\Planedo\Tests\Mocks\SettableClock;
 use DateTimeImmutable;
+use Psr\Clock\ClockInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 trait MockClockTrait
 {
-    public function mockClock(DateTimeImmutable $datetime)
+    public function mockClock(DateTimeImmutable $datetime): ClockInterface
     {
         /** @var KernelInterface $kernel */
         $kernel = self::$kernel;
 
         $clock = new SettableClock($datetime);
         $kernel->getContainer()->set('planedo.clock', $clock);
+
+        return $clock;
     }
 }
