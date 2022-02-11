b591aa715019ea7a1a5f2bec28ad49149aa25c8e
Larry Garfield <larry@garfieldtech.com>
2022-02-11 11:51:52 -0600

    Slightly less bad PHP CS configuration.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,25 +11,19 @@ LICENSE file that was distributed with this source code.
 EOF;
 
 return (new PhpCsFixer\Config())
-    ->setRiskyAllowed(true)
+    ->setRiskyAllowed(false)
     ->setRules([
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
         'concat_space' => ['spacing' => 'one'],
-        'general_phpdoc_annotation_remove' => [
-            'annotations' => ['author']
-        ],
         'header_comment' => [
             'header' => $header
         ],
-        'no_extra_blank_lines' => true,
         'no_superfluous_phpdoc_tags' => false,
-        'php_unit_construct' => [
-            'assertions' => ['assertEquals', 'assertSame', 'assertNotEquals', 'assertNotSame']
-        ],
-        'php_unit_mock_short_will_return' => true,
-        'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
         'declare_strict_types' => true,
+        'protected_to_private' => false,
+        'general_phpdoc_tag_rename' => false,
+        'phpdoc_align' => false,
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
